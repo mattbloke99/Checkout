595b50858bf1de635fad0e9024b7d7024b6ea301
Matthew Hurrell <azure@webfusiontechnology.co.uk>
2021-03-25 18:31:25 +0000

    Created first test for calculating price based on standard unit price

--- Checkout.Core.Tests/UnitTest1.cs
+++ Checkout.Core.Tests/UnitTest1.cs
@@ -6,9 +6,31 @@ namespace Checkout.Core.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CalculateNormalPriceForSingleEntryTest()
         {
+            var sku = new Sku() { Name = "A", Cost = 50 };
+            var entry = new EntryItem() { Quantity = 3, Sku = sku };
 
+            int entryCost = entry.CalculateCost();
+
+            Assert.Equal(150, entryCost);
         }
+    }
+
+    public class EntryItem
+    {
+        public int Quantity { get; set; }
+        public Sku Sku { get; set; }
+
+        public int CalculateCost()
+        {
+            return Quantity * Sku.Cost;
+        }
+    }
+
+    public class Sku
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
     }
 }
