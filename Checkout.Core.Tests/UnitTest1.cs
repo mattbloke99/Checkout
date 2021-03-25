d3b8c1e4da090f5cf8330b4baa580bf773aa8afa
Matthew Hurrell <azure@webfusiontechnology.co.uk>
2021-03-25 18:46:08 +0000

    Added special price test

--- Checkout.Core.Tests/UnitTest1.cs
+++ Checkout.Core.Tests/UnitTest1.cs
@@ -15,6 +15,32 @@ namespace Checkout.Core.Tests
 
             Assert.Equal(150, entryCost);
         }
+
+        [Fact]
+        public void CalculateSpecialPriceForSingleEntryTest()
+        {
+            var specialPrice = new SpecialPrice() { SpecialPriceName = "3 for 130", Quantity = 3, Cost = 130 };
+            var sku = new Sku() { Name = "A", Cost = 50, SpecialPrice = specialPrice };
+            var entry = new EntryItem() { Quantity = 3, Sku = sku };
+
+            int entryCost = entry.CalculateCost();
+
+            Assert.Equal(130, entryCost);
+        }
+
+
+    }
+
+    public class SpecialPrice
+    {
+        //for the purposes of this kata it will be assumed that a sku can only have 0 or 1 special prices
+        public SpecialPrice()
+        {
+        }
+
+        public string SpecialPriceName { get; set; }
+        public int Quantity { get; set; }
+        public int Cost { get; set; }
     }
 
     public class EntryItem
@@ -24,7 +50,13 @@ namespace Checkout.Core.Tests
 
         public int CalculateCost()
         {
-            return Quantity * Sku.Cost;
+            if (Sku.SpecialPrice == null) { 
+                return Quantity * Sku.Cost;
+            } else
+            {
+                int quantityitemsAtSpecialPrice = Quantity / Sku.SpecialPrice.Quantity;
+                return quantityitemsAtSpecialPrice * Sku.SpecialPrice.Cost;
+            }
         }
     }
 
@@ -32,5 +64,6 @@ namespace Checkout.Core.Tests
     {
         public string Name { get; set; }
         public int Cost { get; set; }
+        public SpecialPrice SpecialPrice { get; internal set; }
     }
 }
